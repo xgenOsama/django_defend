b01b21f560c13b64676aeca5f1e6545cd049e33a
robot <xgen.osama>
2016-03-12 15:35:21 +0200

    check concurrent session

--- defend/test_defend.py
+++ defend/test_defend.py
@@ -1,8 +1,23 @@
 from defend.middleware import handling_middleware
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
+from django.core.handlers.base import BaseHandler
 
-factory = RequestFactory()
+
+class RequestMock(RequestFactory):
+    def request(self, **request):
+        "Construct a generic request object."
+        request = RequestFactory.request(self, **request)
+        handler = BaseHandler()
+        handler.load_middleware()
+        for middleware_method in handler._request_middleware:
+            if middleware_method(request):
+                raise Exception("Couldn't create request mock object - "
+                                "request middleware returned a response")
+        return request
+
+
+factory = RequestMock()
 request = factory.get('http://localhost:8000/admin/login/?next=/admin/acunetix/x.bacKup')
 request_method = "FAKE"
 request.user = AnonymousUser()
@@ -12,8 +27,8 @@ request.META['SERVER_NAME'] = "www.fake.com"
 handeling = handling_middleware()
 
 # test checkHttpMethod
-handeling.checkHttpMethod(request, request_method) # takes request and method value
-#handeling.checkHttpMethod(request) # take only request as a parameter
+handeling.checkHttpMethod(request, request_method)  # takes request and method value
+# handeling.checkHttpMethod(request) # take only request as a parameter
 
 
 # test checkURI method
@@ -30,3 +45,8 @@ handeling.checkHostname(request, 'www.example.com')
 
 # test nonExistingFile takes only request as a parameter
 handeling.nonExistingFile(request)
+
+# test check concurrent session
+request.session['REMOTE_ADDR'] = "127.0.0.1"
+request.META['REMOTE_ADDR'] = "127.0.0.2"
+handeling.checkConcurrentSession(request)
